471f4b728f8ab2d4117a0252bfd813a41185beb9
mbusel04 <mbusel@ucsd.edu>
2023-10-24 13:48:28 -0700

    Lab 3 - in progress

--- ArrayTests.java
+++ ArrayTests.java
@@ -1,14 +1,18 @@
 import static org.junit.Assert.*;
+
+import java.util.ArrayList;
+import java.util.List;
+
 import org.junit.*;
 
 public class ArrayTests {
 
-    @Test
-    public void testReversedInPlaceMultiple() {
-      int[] input1 = { 1, 2, 3 , 4};
-      ArrayExamples.reverseInPlace(input1);
-      assertArrayEquals(new int[]{4, 3, 2, 1 }, input1);
-    }
+  @Test
+  public void testReversedInPlaceMultiple() {
+    int[] input1 = { 1, 2, 3 , 4};
+    ArrayExamples.reverseInPlace(input1);
+    assertArrayEquals(new int[]{4, 3, 2, 1 }, input1);
+  }
 
 	@Test 
 	public void testReverseInPlace() {
@@ -34,5 +38,51 @@ public class ArrayTests {
   public void testReversedEmpyt() {
     int[] input1 = { };
     assertArrayEquals(new int[]{ }, ArrayExamples.reversed(input1));
+  }
+
+  // @Test
+  // public void testAvarege() {
+  //   double[] input1 = { -1, 1, 5, 6, 1, -1};
+  //   assertEquals(2.4, ArrayExamples.averageWithoutLowest(input1), 0.001); 
+  // }
+
+  @Test
+  public void testListTrueFilter() {
+    List<String> input1 = new ArrayList<String>();
+    input1.add("a");
+    input1.add("b");
+    input1.add("c");
+    assertEquals(input1, ListExamples.filter(input1, new AlwaysTrueTester()));
+  }
+
+  @Test
+  public void testListConditionalFilter() {
+    List<String> input1 = new ArrayList<String>();
+    input1.add("a");
+    input1.add("ab");
+    input1.add("b");
+    List<String> outpu1 = new ArrayList<String>();
+    outpu1.add("a");
+    outpu1.add("b");
+    assertEquals(outpu1, ListExamples.filter(input1, new SingleCharacterTester()));
+  }
+}
+
+class AlwaysTrueTester implements StringChecker {
+  
+  @Override
+  public boolean checkString(String s) {
+     return true;
+  }
+}
+
+class SingleCharacterTester implements StringChecker {
+  
+  @Override
+  public boolean checkString(String s) {
+    if (s.length() == 1) {
+      return true;
+    }
+     return false;
   }
 }
